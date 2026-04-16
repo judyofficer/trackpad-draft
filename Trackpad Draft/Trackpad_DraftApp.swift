feb1defbe1b7fed53d59bfb57f63824e598e7071
judyofficer <2695333577@qq.com>
2026-04-16 22:42:12 +0800

    Initial Commit

---
+++ Trackpad Draft/Trackpad_DraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trackpad_DraftApp.swift
+//  Trackpad Draft
+//
+//  Created by aesir on 2026/4/16.
+//
+
+import SwiftUI
+
+@main
+struct Trackpad_DraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
